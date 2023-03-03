c38e8a98ff533d330b705c9086e20ef2e5eb72cb
Jiahua Xu <xujiahuayz@gmail.com>
2023-03-03 22:40:08 +0000

    update computation

---
+++ playground/plot_market.py
@@ -0,0 +1,29 @@
+"""
+plot market with boom bust cycles
+"""
+
+from environ.process.data.sp import sp_df
+from environ.utils.computations import boom_bust
+import matplotlib.pyplot as plt
+import matplotlib.dates as md
+
+# convert date to timestamp
+sp_df["time"] = sp_df["Date"].apply(md.date2num)
+
+# replace s&p colume with price
+sp_df = sp_df.rename(columns={"S&P": "price"})
+
+BOOM_BUST = boom_bust(sp_df)
+
+# plot price with boom bust cycles
+plt.plot(sp_df["time"], sp_df["price"])
+for i in BOOM_BUST["boom"]:
+    plt.axvspan(i[0], i[1], color="green", alpha=0.3)
+for i in BOOM_BUST["bust"]:
+    plt.axvspan(i[0], i[1], color="red", alpha=0.3)
+
+# format the x axis
+plt.gca().xaxis.set_major_formatter(md.DateFormatter("%Y-%m-%d"))
+plt.gcf().autofmt_xdate()
+
+plt.show()
