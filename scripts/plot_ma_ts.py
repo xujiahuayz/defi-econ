af2d985b5c876ef74094d0a4a9f93cbe78530f26
Yichen Luo <103887158+lyc0603@users.noreply.github.com>
2023-03-24 18:07:57 +0000

    delete unnecessary code

--- scripts/plot_ma_ts.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from environ.constants import TABLE_PATH
-from environ.plot.plot_ma import preprocess_ma, plot_time_series
-
-
-regression_panel = pd.read_pickle(TABLE_PATH / "reg_panel.pkl")
-
-ma_df = preprocess_ma(df=regression_panel, value_colume="Volume_share", ma_window=30)
-plot_time_series(ma_df)
