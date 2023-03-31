405ba80e2905ba1bf04dfe5c09fda8f848d8db5d
xujiahuayz <xujiahuayz@gmail.com>
2023-03-31 15:55:36 +0100

    update with AAVE data

--- environ/tabulate/render_panel_event_regression.py
+++ environ/tabulate/render_panel_event_regression.py
@@ -16,13 +16,15 @@ def panel_event_regression(
     lead_lag_interval: int | None = None,
     reltime_dummy: str = "rel_time",
     dummy_prefix_sep: str = "_",
+    # TODO: consider removing this variable
+    treatment_delay: int = 0,
     **kwargs,
 ) -> pd.DataFrame:
 
     diff_in_diff_df[reltime_dummy] = diff_in_diff_df["lead_lag"].map(
         lambda x: (str(int(x // lead_lag_interval)) if -window <= x <= window else "NA")
         if lead_lag_interval
-        else (str(int(x >= 7)) if x >= 7 else "NA")
+        else (str(int(x >= treatment_delay)) if x >= treatment_delay else "NA")
     )
 
     # add time_to_join as a categorical variable
@@ -62,19 +64,15 @@ def panel_event_regression(
         if obs_start_date >= diff_in_diff_df["Date"].min():
 
             did_reg_panel = diff_in_diff_df.loc[
-                (diff_in_diff_df["Date"] >= obs_start_date)
+                (diff_in_diff_df["Date"] >= obs_start_date - treatment_delay)
                 & (diff_in_diff_df["Date"] <= obs_end_date)
             ]
 
             sum_dummies_grouped = did_reg_panel.groupby("Token")["has_been_treated"]
 
             # select control group
-            sum_dummies = (
-                (sum_dummies_grouped.prod() == 1)
-                |  # already treated
-                # if control_with_treated
-                # else
-                (sum_dummies_grouped.sum() == 0)  # never treated
+            sum_dummies = (sum_dummies_grouped.prod() == 1) | (
+                sum_dummies_grouped.sum() == 0
             )
 
             additional_tokens = set(sum_dummies[sum_dummies].index)
@@ -87,17 +85,24 @@ def panel_event_regression(
                     "Token",
                 ]
             )
+
+            # remove tokens with time_to_treat_cols != 0
+
             did_reg_panel = did_reg_panel.loc[
                 did_reg_panel["Token"].isin(additional_tokens | treated_tokens)
+                & (did_reg_panel["Date"] >= obs_start_date)
             ]
             # TODO: hanlde the below when a token is added exactly at the beginning of the window
             # assert (
             #     did_reg_panel.loc[~did_reg_panel["Token"].isin(treated_tokens), time_to_treat_cols].sum().sum()
             #     == 0
             # ), "untreated tokens should have 0 in time_to_treat_cols"
-            did_reg_panel.loc[
-                ~did_reg_panel["Token"].isin(treated_tokens), time_to_treat_cols
-            ] = 0
+            # TODO: too implicit, need to make it more explicit
+            if lead_lag_interval is None:
+                # doing the binary style, need interactive variable
+                did_reg_panel.loc[
+                    ~did_reg_panel["Token"].isin(treated_tokens), time_to_treat_cols
+                ] = 0
             did_reg_panel_full = pd.concat([did_reg_panel_full, did_reg_panel], axis=0)
 
     did_result = render_regress_table(
--- scripts/regress_diff_in_diff_event_stack.py
+++ scripts/regress_diff_in_diff_event_stack.py
@@ -1,5 +1,6 @@
 # get the regression panel dataset from pickled file
 import re
+import numpy as np
 
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -7,6 +8,7 @@ from matplotlib import pyplot as plt
 from environ.constants import (
     ALL_NAMING_DICT,
     COMPOUND_DEPLOYMENT_DATE,
+    AAVE_DEPLOYMENT_DATE,
     DATA_PATH,
     DEPENDENT_VARIABLES,
     SAMPLE_PERIOD,
@@ -15,17 +17,27 @@ from environ.constants import (
 from environ.tabulate.render_panel_event_regression import panel_event_regression
 from environ.tabulate.render_regression import render_regress_table_latex
 
-compound_date = pd.DataFrame(
-    {
-        "Token": v["Token"] if v["Token"] != "ETH" else "WETH",
-        "join_compound_time": pd.to_datetime(v["Date"].split(" ")[0]),
-    }
-    for v in COMPOUND_DEPLOYMENT_DATE
-)
+# combine AAVE and COMPOUND deployment date list
+plf_list = AAVE_DEPLOYMENT_DATE + COMPOUND_DEPLOYMENT_DATE
+# change the format of each element in the list to {'DAI': ['2019-05-07 01:20:54', '2019-05-07 01:20:54']}
 
-compound_date["join_compound_time"] = compound_date["join_compound_time"].astype(
-    int
-) // (10**9 * 3600 * 24 * 1)
+name_map = {"ETH": "WETH", "WBTC2": "WBTC"}
+plf_dict: dict[str, list] = {}
+for v in plf_list:
+    # replace the token name 'ETH' with 'WETH' and WBTC2 with WBTC
+    token = name_map.get(v["Token"], v["Token"])
+    # convert date from string to timestamp
+    date = int(pd.to_datetime(v["Date"]).timestamp()) // (3600 * 24 * 1)
+    if token in plf_dict:
+        plf_dict[token].append(date)
+    else:
+        plf_dict[token] = [date]
+
+# get the earliest date for each token
+plf_date = pd.DataFrame(
+    {"Token": k, "earliest_join_time": min(v), "join_time_list": v}
+    for k, v in plf_dict.items()
+)
 
 
 reg_panel = pd.read_pickle(DATA_PATH / "processed" / "reg_panel_new.pkl")
@@ -37,29 +49,39 @@ reg_panel = reg_panel[
 
 reg_panel["Date"] = reg_panel["Date"].astype(int) // (10**9 * 3600 * 24 * 1)
 # average by Date and Token
-reg_panel = reg_panel.groupby(["Date", "Token"]).mean().reset_index()
+reg_panel = reg_panel.groupby(["Date", "Token"]).mean(numeric_only=True).reset_index()
 
-reg_panel = reg_panel.merge(compound_date, on="Token", how="left")
+reg_panel = reg_panel.merge(plf_date, on="Token", how="left")
 
 
 RELTIME_DUMMY = "rel_time"
 FACTOR_PREFIX = "_"
 
-all_added_dates = set(
-    # take only the date in '2019-05-07 01:20:54' without time
-    compound_date["join_compound_time"]
-)
-
+all_added_dates = set(plf_date["join_time_list"].sum())
 
 diff_in_diff_df = reg_panel.loc[:, ["Token", "Date"] + DEPENDENT_VARIABLES]
 
-diff_in_diff_df["lead_lag"] = reg_panel["Date"] - reg_panel["join_compound_time"]
+
+def lead_lag(date: float, join_time_list: list[float]) -> float:
+    # if join_time_list is not NaN
+    if join_time_list is not np.nan:
+        return date - min(join_time_list, key=lambda d: abs(d - date))
+    else:
+        return np.nan
+
+
+# lead_lag is the time difference between the date of the observation and the date of the nearest treatment from join_time_list
+diff_in_diff_df["lead_lag"] = reg_panel.apply(
+    lambda row: lead_lag(row["Date"], row["join_time_list"]), axis=1
+)
+
+diff_in_diff_df["lead_lag"] = reg_panel["Date"] - reg_panel["earliest_join_time"]
 
 diff_in_diff_df["has_been_treated"] = diff_in_diff_df["lead_lag"] >= 0
 
 did_result = panel_event_regression(
     diff_in_diff_df=diff_in_diff_df,
-    window=14,
+    window=7 * 3,
     control_with_treated=True,
     # lead_lag_interval=7,
     reltime_dummy=RELTIME_DUMMY,
@@ -67,6 +89,7 @@ did_result = panel_event_regression(
     standard_beta=False,
     panel_index_columns=(["Token", "Date"], [True, True]),
     robust=False,
+    treatment_delay=0,
 )
 
 # get index that contains RELTIME_DUMMY
@@ -74,7 +97,7 @@ time_to_treat_cols = [k for k in did_result.index if RELTIME_DUMMY in k]
 
 did_result_latex = render_regress_table_latex(
     result_table=did_result,
-    file_name=TABLE_PATH / "did_event_stack.tex",
+    file_name=TABLE_PATH / "did_event_stack",
 )
 
 # get all the rows with index in time_to_treat_cols, ignore items in time_to_treat_cols that are not in the index
@@ -97,15 +120,14 @@ plot_df_se = (
 
 x = plot_df["time_to_join"]
 # plot the result
-for k, v in did_result.loc["regressand"].iteritems():
-    plt.plot(x, plot_df_co[k], label=f"${ALL_NAMING_DICT[v]}$")
-    # stop after the first one
+for k, v in did_result.loc["regressand"].items():
+    plt.plot(x, plot_df_co[k], label=f"${ALL_NAMING_DICT[v]}$")  # type: ignore
 
     # plot the standard error band
     plt.fill_between(
         x,
-        plot_df_co[k] - 1.96 * plot_df_se[k],
-        plot_df_co[k] + 1.96 * plot_df_se[k],
+        plot_df_co[k] - 1.96 * plot_df_se[k],  # type: ignore
+        plot_df_co[k] + 1.96 * plot_df_se[k],  # type: ignore
         alpha=0.2,
     )
 plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0.0)
@@ -115,6 +137,6 @@ plt.axvline(x=0, color="black", linestyle="--")
 # plot horizontal line at 0
 plt.axhline(y=0, color="black", linestyle="--")
 # add x axis label
-plt.xlabel("Time to join Compound (weeks)")
+plt.xlabel("Time to be included in any PLF (weeks)")
 plt.show()
 plt.close()
