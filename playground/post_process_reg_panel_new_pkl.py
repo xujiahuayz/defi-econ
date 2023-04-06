72a0004272b423059e1243d8ab5f540603c29a84
Jiahua Xu <xujiahuayz@gmail.com>
2023-04-06 07:38:37 +0100

    preprocess pkl data

--- playground/post_process_reg_panel_new_pkl.py
+++ playground/post_process_reg_panel_new_pkl.py
@@ -1,29 +1,37 @@
-"""
-Plot (auto)correlation and (auto)covariance matrices
-"""
-
 import pandas as pd
 
 from environ.constants import DATA_PATH, DEPENDENT_VARIABLES
 
 reg_panel = pd.read_pickle(DATA_PATH / "processed" / "reg_panel_merged.pkl")
 
-eigen_centrality_undirected = "eigen_centrality_undirected"
+unprocessed_dvs = ["eigen_centrality_undirected"]
 
 var_without_na = [
     "Inflow_centrality_swap",
     "Outflow_centrality_swap",
-] + list(set(DEPENDENT_VARIABLES) - {eigen_centrality_undirected})
+    "vol_in_full_len",
+    "vol_out_full_len",
+] + list(set(DEPENDENT_VARIABLES) - set(unprocessed_dvs))
 reg_panel[var_without_na] = reg_panel[var_without_na].fillna(0)
 
 # take the average of Inflow_centrality and Outflow_centrality for eigen_centrality_undirected
-reg_panel[eigen_centrality_undirected] = (
+reg_panel["eigen_centrality_undirected"] = (
     reg_panel["Inflow_centrality_swap"] + reg_panel["Outflow_centrality_swap"]
 ) / 2
+
+reg_panel["vol_total_full_len"] = (
+    reg_panel["vol_in_full_len"] + reg_panel["vol_out_full_len"]
+)
+# calculate each token's share of vol_total_full_len by day
+# group by day first
+reg_panel["vol_total_full_len_share"] = reg_panel.groupby("Date")[
+    "vol_total_full_len"
+].transform(lambda x: x / x.sum())
 
 
 # TODO: check why this is necessary -- why some eigenvector centrality values are negative?
 reg_panel[DEPENDENT_VARIABLES] = reg_panel[DEPENDENT_VARIABLES].clip(lower=0)
+
 
 # repickle the reg_panel
 reg_panel.to_pickle(DATA_PATH / "processed" / "reg_panel_merged.pkl")
