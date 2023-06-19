03a1a1edec70f685ee91c1975793673420f469e1
JX <jx@eduroam-int-dhcp-97-53-202.ucl.ac.uk>
2023-06-19 16:32:24 +0100

    make portfolio.py run

--- scripts/process/portfolio.py
+++ scripts/process/portfolio.py
@@ -4,13 +4,13 @@ Script to implement asset pricing using dominance as indicator
 
 import pandas as pd
 from environ.process.asset_pricing.double_sorting import (
-    stable_nonstable_split,
     asset_pricing,
 )
 from environ.constants import (
     PROCESSED_DATA_PATH,
     FIGURE_PATH,
     DEPENDENT_VARIABLES,
+    STABLE_DICT,
 )
 
 
@@ -19,13 +19,9 @@ reg_panel = pd.read_pickle(
     PROCESSED_DATA_PATH / "panel_main.pickle.zip", compression="zip"
 )
 
-# split the dataframe into stablecoin and non stablecoin
-df_panel_stablecoin, df_panel_nonstablecoin = stable_nonstable_split(reg_panel)
-
-# stable non-stable info dict
 stable_nonstable_info = {
-    "stablecoin": df_panel_stablecoin,
-    "nonstablecoin": df_panel_nonstablecoin,
+    "stablecoin": reg_panel[reg_panel["Token"].isin(STABLE_DICT.keys())],
+    "nonstablecoin": reg_panel[~reg_panel["Token"].isin(STABLE_DICT.keys())],
 }
 
 # iterate through the dominance
