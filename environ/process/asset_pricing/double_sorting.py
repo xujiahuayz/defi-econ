f176a1aab3b44c09c131d2c51ec8f2e363cd33ef
ben <b.chen22@lse.ac.uk>
2025-02-21 23:46:36 +0000

    add weekly portfolios formation

--- environ/process/asset_pricing/double_sorting.py
+++ environ/process/asset_pricing/double_sorting.py
@@ -237,3 +237,65 @@ def asset_pricing(
 
     # evaluate the performance of the portfolio
     return _eval_port(pd.DataFrame(ret_dict), freq, n_port)
+
+
+def build_weekly_portfolios(
+    reg_panel: pd.DataFrame,
+    brk_pt_lst: list[float],
+    dominance_var: str = "volume_ultimate_share",
+    zero_value_portfolio: bool = True,
+    simple_dollar_ret: bool = False,
+) -> pd.DataFrame:
+    """
+    Aggregate function to create portfolios by week of the year
+    """
+
+    n_port = len(brk_pt_lst) + 2 if zero_value_portfolio else len(brk_pt_lst) + 1
+    df_panel = calculate_period_return(
+        df_panel=reg_panel, freq=7, simple_dollar_ret=simple_dollar_ret
+    )
+
+    # Add columns for the week and year
+    df_panel['Week'] = df_panel['Date'].dt.isocalendar().week
+    df_panel['Year'] = df_panel['Date'].dt.isocalendar().year
+
+    # Prepare the dataframe to store the portfolio
+    week_year_list = df_panel[['Year', 'Week']].drop_duplicates().sort_values(by=['Year', 'Week']).values.tolist()
+
+    # Dict to store the weekly portfolio return
+    ret_dict = {f"P{port}": [] for port in range(1, n_port + 1)}
+    ret_dict["Year"] = []
+    ret_dict["Week"] = []
+
+    df_panel = lag_variable_columns(
+        data=df_panel,
+        variable=[dominance_var, REFERENCE_DOM],
+        time_variable="Date",
+        entity_variable="Token",
+    )
+
+    # Loop through the weeks
+    for year, week in week_year_list:
+        # Asset pricing
+        df_panel_week = df_panel[(df_panel['Year'] == year) & (df_panel['Week'] == week)].copy()
+        ret_dict = _sorting(
+            df_panel_period=df_panel_week,
+            risk_factor=dominance_var,
+            zero_value_portfolio=zero_value_portfolio,
+            ret_dict=ret_dict,
+            brk_pt_lst=brk_pt_lst,
+        )
+        ret_dict["Year"].append(year)
+        ret_dict["Week"].append(week)
+
+    df_ret = pd.DataFrame(ret_dict)
+    df_ret.sort_values(by=["Year", "Week"], ascending=True, inplace=True)
+
+    # Annualize return
+    for portfolio in [f"P{port}" for port in range(1, n_port + 1)]:
+        df_ret[portfolio] = df_ret[portfolio] * 52  # 52 weeks in a year
+
+    # Calculate the bottom minus top
+    df_ret[f"P{n_port} - P1"] = df_ret[f"P{n_port}"] - df_ret["P1"]
+
+    return df_ret
